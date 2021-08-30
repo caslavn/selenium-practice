64e4f383ebd7a553c4ed00a7c2eb1ddac0f7c262
ValentinaCvij <valentina.cvijovic@dev.telnet.rs>
2021-08-30 13:21:08 +0200

    Solved login exception and browser window hidden

--- src/main/java/com/example/seleniumpractice/LoginUsingSelenium.java
+++ src/main/java/com/example/seleniumpractice/LoginUsingSelenium.java
@@ -1,36 +1,27 @@
 package com.example.seleniumpractice;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-import org.testng.Assert;
+
 
 public class LoginUsingSelenium {
 
-
     @Test
     public void login() {
-
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromerdriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://www.schulung.simap.ch/");
-
+        driver.get("https://www.schulung.simap.ch/shabforms/COMMON/login/login.jsf?view=3&GO=Projectmanager");
         WebElement username=driver.findElement(By.id("userlogin"));
         WebElement password=driver.findElement(By.id("userpwd"));
-        WebElement login=driver.findElement(By.xpath("//*[@id=\"login\"]/input[3]"));
-
+        WebElement login=driver.findElement(By.name("command"));
         username.sendKeys("olmero-test");
         password.sendKeys("jbZVF659aNYrkMp");
         login.click();
-
-        //String actualUrl="";
-        //String expectedUrl= driver.getCurrentUrl();
-
-        //Assert.assertEquals(expectedUrl,actualUrl);
-
-
     }
 }
